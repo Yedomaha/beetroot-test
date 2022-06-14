9b53c59e37691116c74619a8e0e69f95436a2988
Yan <yedomaha3@gmail.com>
2022-06-14 11:02:45 +0300

    Job openings section (html in progress)

--- wp-content/themes/beetroot-wp-test/front-page.php
+++ wp-content/themes/beetroot-wp-test/front-page.php
@@ -13,5 +13,36 @@
  */
 get_header(); ?>
 
+    <main>
+
+        <section class="job-openings">
+            <div class="container">
+                <div class="job-openings__title-area">
+                    <h1 class="job-openings__title">Join the team</h1>
+                    <div class="job-openings__text">A career at Beetroot is for those who are passionate and curious.
+                        It's a home
+                        for those who take responsibility and approach life with the right attitude.
+                    </div>
+                </div>
+                <div class="job-filter">
+                    <div class="search-bar job-filter__search-bar">
+                        <div class="search-bar__input-wrap"></div>
+                        <div class="search-bar__drop-down-wrap"></div>
+                        <div class="search-bar__drop-down-wrap"></div>
+                    </div>
+                    <ul class="tags-list job-filter__tags-list">
+                        <li class="tags-list__item"></li>
+                        <li class="tags-list__item"></li>
+                        <li class="tags-list__item"></li>
+                        <li class="tags-list__item tags-list__item--grid-switcher"></li>
+                    </ul>
+                    <div class="job-filter__grid">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
 
 <?php get_footer(); ?>
